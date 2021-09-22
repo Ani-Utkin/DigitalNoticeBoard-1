9603241599834d0267897e2a95d308e4aa766abc
Ani-Utkin <60753926+Ani-Utkin@users.noreply.github.com>
2021-09-21 22:38:38 -0400

    created react app

---
+++ digital-noticeboard/src/App.js
@@ -0,0 +1,18 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <header className="App-header">
+              <div>Public</div>
+              <div>Group 1</div>
+              <div>Group 2</div>
+              <div>Profile Picture</div>
+          </header>
+
+          <main>This is the main page</main>
+    </div>
+  );
+}
+
+export default App;
