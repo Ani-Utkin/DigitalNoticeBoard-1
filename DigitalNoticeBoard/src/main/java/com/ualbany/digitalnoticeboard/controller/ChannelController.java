a1561da5714776aae29522557b42a400b686d76d
Vijay-yeruva <Vijayasekhar.yeruva@gmail.com>
2021-11-06 11:00:54 -0400

    fixed the public page lick from other places

--- DigitalNoticeBoard/src/main/java/com/ualbany/digitalnoticeboard/controller/ChannelController.java
+++ DigitalNoticeBoard/src/main/java/com/ualbany/digitalnoticeboard/controller/ChannelController.java
@@ -8,11 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ualbany.digitalnoticeboard.model.Channel;
@@ -36,8 +35,8 @@ public class ChannelController {
 	@Autowired
 	ShortNoticeService shortNoticeService;
 	
-	@RequestMapping(value = "/{username}/channel/add", method = RequestMethod.GET)
-    public ModelAndView getEmployeeById(@PathVariable final String username, @ModelAttribute("channelForm") Channel channelForm, BindingResult bindingResult,  Model model) {
+	@GetMapping(value = "/{username}/channel/add")
+    public ModelAndView channelAddGet(@PathVariable final String username, @ModelAttribute("channelForm") Channel channelForm, BindingResult bindingResult,  Model model) {
 		User user = userService.findByUsername(username);
 	    ModelAndView mv = new ModelAndView("addchannel");
 	    List<Visibility> visibilityOptions = new ArrayList<Visibility>();
@@ -49,7 +48,7 @@ public class ChannelController {
     }
 	 
 	@PostMapping("/{username}/channel/add")
-    public ModelAndView profileGet(@PathVariable final String username, @ModelAttribute("channelForm") Channel channelForm, BindingResult bindingResult, Model model) {
+    public ModelAndView channelAddPut(@PathVariable final String username, @ModelAttribute("channelForm") Channel channelForm, BindingResult bindingResult, Model model) {
 		User user = userService.findByUsername(username);
 		setpersistableproperties(channelForm, user);
         channelService.save(channelForm);
--- DigitalNoticeBoard/src/main/java/com/ualbany/digitalnoticeboard/controller/HomeController.java
+++ DigitalNoticeBoard/src/main/java/com/ualbany/digitalnoticeboard/controller/HomeController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ualbany.digitalnoticeboard.model.Channel;
 import com.ualbany.digitalnoticeboard.model.ShortNotice;
+import com.ualbany.digitalnoticeboard.model.User;
 import com.ualbany.digitalnoticeboard.service.ChannelService;
 import com.ualbany.digitalnoticeboard.service.ShortNoticeService;
+import com.ualbany.digitalnoticeboard.service.UserService;
 
 @Controller
 public class HomeController {
@@ -22,9 +25,24 @@ public class HomeController {
 	@Autowired
 	ShortNoticeService shortNoticeService;
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping("/home")
-    public ModelAndView profileGet(Model model) {
+    public ModelAndView homeGet(Model model) {
         ModelAndView mv = new ModelAndView("home");
+        List<Channel> channels = channelService.getAllPublicChannels();
+        mv.addObject("Channels", channels);
+        List<ShortNotice> shortnotices = shortNoticeService.getAllPublicNotices();
+        mv.addObject("ShortNotices", shortnotices);
+        return mv;
+    }
+	
+	@GetMapping(value = "/{username}/home")
+    public ModelAndView userHomeGet(@PathVariable final String username, Model model) {
+		User user = userService.findByUsername(username);
+		ModelAndView mv = new ModelAndView("userhome");
+		mv.addObject("user", user);
         List<Channel> channels = channelService.getAllPublicChannels();
         mv.addObject("Channels", channels);
         List<ShortNotice> shortnotices = shortNoticeService.getAllPublicNotices();
